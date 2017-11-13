f883dc2decbab7dd52259b0af1d8c25d922eefe2
will0 <93719952@qq.com>
2017-11-13 13:33:56 +0800

    11

--- crm/test01/urls.py
+++ crm/test01/urls.py
@@ -7,5 +7,14 @@ from django.conf.urls import url
 from test01 import views
 
 urlpatterns = [
-    url(r'^index/', views.index),
+    url(r'^index/$', views.index),
+    url(r'^student_index/$', views.student_index,name='student_index'),
+    url(r'^teacher_index/$', views.teacher_index,name='teacher_index'),
+    url(r'^customer_index/$', views.customer_index,name='customer_index'),
+    url(r'^sale_index/$', views.sale_index,name='sale_index'),
 ]
+
+# 销售 sale_index customer_index
+# 老师 teacher_index
+# 主任 sale_index student_index leader_index teacher_index customer_index
+# 学生 student_index
--- crm/test01/views.py
+++ crm/test01/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print(type(request.user))
     return render(request,'index.html')
+
+
+def student_index(request):
+    return render(request,'student_index.html')
+
+
+def sale_index(request):
+    return render(request,'sale_index.html')
+
+def customer_index(request):
+    return render(request,'customer_index.html')
+
+def teacher_index(request):
+    return render(request,'teacher_index.html')
